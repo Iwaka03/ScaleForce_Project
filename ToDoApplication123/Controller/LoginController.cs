b52c162c765ae3af297a9be2208d5400d1e31c63
Iwaka03 <78343885+Iwaka03@users.noreply.github.com>
2021-05-27 11:09:58 +0300

    Add files via upload

---
+++ ToDoApplication123/Controller/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using ToDoApplication123.Model;
+
+namespace ToDoApplication.Controller
+{
+    public class LoginController
+    {
+        public void InsertUser(User user)
+        {
+            using (ToDoEntities toDo = new ToDoEntities())
+            {
+                toDo.Users.Add(user);
+                toDo.SaveChanges();
+            }
+        }
+
+        public bool IsUserRegistered(string username, string password)
+        {
+            using (ToDoEntities toDo = new ToDoEntities())
+            {
+                bool isRegistered = false;
+                foreach(var user in toDo.Users)
+                {
+                    if(user.Username == username && user.Password == password)
+                    {
+                        isRegistered = true;
+                    }
+                }
+                return isRegistered;
+            }
+        }
+    }
+}
